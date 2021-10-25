2e7a79d2bb4cda70596bb3ea4bff041daf426a3d
Arjan Kuiper <a.kuiper@emit-it.com>
2021-10-25 21:41:37 +0200

    Remove build folder

--- build/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function tes(str: string): string[];
--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tes = void 0;
-function tes(str) {
-    console.log(str);
-    return [];
-}
-exports.tes = tes;
